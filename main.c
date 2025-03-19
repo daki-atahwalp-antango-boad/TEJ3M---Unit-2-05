7f9c27903d0a8c3fa5470e329a914dc5d8ca9b00
Daki <144264104+daki-atahwalp-antango-boad@users.noreply.github.com>
2025-03-19 09:25:03 -0400

    Create main.c

---
+++ main.c
@@ -0,0 +1,32 @@
+/* 
+ *This program turns a servo 180 to 0 degrees every second
+ *
+ *Created on Feb 2025
+ *By Daki A.B
+*/ 
+
+#include <Servo.h>
+Servo servo;
+
+// setting constants
+const int PIN_7 = 7;
+const int TURN_TIME = 1000;
+
+// setting variable value
+int servoAngle = 0;
+
+void setup() {
+  // assigning servo pins to arduino
+  servo.attach(7);
+  servo.write(servoAngle);
+}
+
+void loop() {
+  // turning servo 180 degrees, pause, turning to 0 degrees
+  servoAngle += 180;
+  servo.write(servoAngle);
+  delay(TURN_TIME);
+  servoAngle -= 180;
+  servo.write(servoAngle);
+  delay(TURN_TIME);
+}
